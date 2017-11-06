9b8c4e7964b6f5afa3a43830fd69a88ca564b458
KennethLloyd <kpverastigue@up.edu.ph>
2017-11-06 10:26:54 -0300

    Sample chat v2

--- Doodler/src/GreetingClient.java
+++ Doodler/src/GreetingClient.java
@@ -1,31 +1,43 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class GreetingClient{
   public static void main(String [] args){
-    String serverName = args[0];
-    int port = Integer.parseInt(args[1]);
+    int counter = 0;
+    while (true) {
+      try {
+        String serverName = args[0];
+        int port = Integer.parseInt(args[1]);
+        Socket client;
+        
+        if (counter == 0) {
+          /* Open a ClientSocket and connect to ServerSocket */
+          System.out.println("Connecting to " + serverName + " on port " + port);
+          counter++;
+        }
+        client = new Socket(serverName, port);
 
-    try
-    {
-      /* Open a ClientSocket and connect to ServerSocket */
-      System.out.println("Connecting to " + serverName + " on port " + port);
-      Socket client = new Socket(serverName, port);
-
-      /* Send data to the ServerSocket */
-      System.out.println("Just connected to " + client.getRemoteSocketAddress());
-      OutputStream outToServer = client.getOutputStream();
-      DataOutputStream out = new DataOutputStream(outToServer);
-      out.writeUTF("Hello from " + client.getLocalSocketAddress());
-
-      /* Receive data from the ServerSocket */
-      InputStream inFromServer = client.getInputStream();
-      DataInputStream in = new DataInputStream(inFromServer);
-      System.out.println("Server says " + in.readUTF());
-      client.close();
-
-    }catch(IOException e){
-      e.printStackTrace();
+        /* Send data to the ServerSocket */
+          //System.out.println("Just connected to " + client.getRemoteSocketAddress());
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+          
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Client: ");
+        String msg = sc.nextLine();
+          
+        out.writeUTF("Client: " + msg);
+        
+        /* Receive data from the ServerSocket */
+        InputStream inFromServer = client.getInputStream();
+        DataInputStream in = new DataInputStream(inFromServer);
+        System.out.println(in.readUTF());
+          
+        client.close();
+      }
+      catch(IOException e){
+          e.printStackTrace();
+      }
     }
   }
 }
--- Doodler/src/GreetingServer.java
+++ Doodler/src/GreetingServer.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class GreetingServer extends Thread{
@@ -6,36 +7,42 @@ public class GreetingServer extends Thread{
 
   public GreetingServer(int port) throws IOException{
     serverSocket = new ServerSocket(port);
-    serverSocket.setSoTimeout(500000);
+    serverSocket.setSoTimeout(100000);
   }
 
   public void run(){
+    System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
     while(true){
-      try
-      {
-        System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
-
+        try
+        {
         /* Start accepting data from the ServerSocket */
         Socket server = serverSocket.accept();
-        System.out.println("Just connected to " + server.getRemoteSocketAddress());
-
-        /* Read data from the ClientSocket */
-        DataInputStream in = new DataInputStream(server.getInputStream());
-        System.out.println(in.readUTF());
-
-        DataOutputStream out = new DataOutputStream(server.getOutputStream());
-
-        /* Send data to the ClientSocket */
-        out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
-        server.close();
-
-      }catch(SocketTimeoutException s){
-        System.out.println("Socket timed out!");
-        break;
-      }catch(IOException e){
-        e.printStackTrace();
-        break;
-      }
+        //System.out.println("Just connected to " + server.getRemoteSocketAddress());
+          /* Read data from the ClientSocket */
+          DataInputStream in = new DataInputStream(server.getInputStream());
+          System.out.println(in.readUTF());
+  
+          DataOutputStream out = new DataOutputStream(server.getOutputStream());
+          
+          Scanner sc = new Scanner(System.in);
+          System.out.print("Server: ");
+          String msg = sc.nextLine();
+          
+          out.writeUTF("Server: " + msg);
+          
+          if (msg.equals("bye") || msg.equals("BYE") || msg.equals("Bye")) {
+            /* Send data to the ClientSocket */
+            out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
+            server.close();
+          }
+  
+        }catch(SocketTimeoutException s){
+          System.out.println("Socket timed out!");
+          break;
+        }catch(IOException e){
+          e.printStackTrace();
+          break;
+        }
     }
   }
 
