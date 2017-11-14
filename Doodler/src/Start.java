82fce238cc5abb47fd2276a8d4117f277a9f6fa4
KennethLloyd <kpverastigue@up.edu.ph>
2017-11-14 02:31:18 -0300

    change start swt to swing

--- Doodler/src/Start.java
+++ Doodler/src/Start.java
@@ -1,31 +1,42 @@
-import org.eclipse.swt.widgets.Display;
+/*import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseEvent;*/
 
-public class Start {
-	private static Text port;
-	private static Text ip;
-	private static Text uname;
+import javax.swing.*;
 
+import java.awt.*;
+import java.awt.event.*;
+
+public class Start implements MouseListener {
 	/**
 	 * Launch the application.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Display display = Display.getDefault();
+		/*Display display = Display.getDefault();
 		Shell shell = new Shell();
-		shell.setSize(262, 228);
-		shell.setText("SWT Application");
 		
-		Button btnNewButton = new Button(shell, SWT.NONE);
+		shell.setSize(262, 228);
+		shell.setText("SWT Application");*/
+		JTextField port = new JTextField(10);
+		JTextField ip = new JTextField(10);
+		final JTextField uname = new JTextField(10);
+		
+		JFrame frame = new JFrame("Start");
+		frame.setPreferredSize(new Dimension(262,228));
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLayout(new FlowLayout());
+		Container container = frame.getContentPane();
+		
+		JButton btnNewButton = new JButton("Connect");
 		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseDown(MouseEvent e) {
+			public void mouseClicked(MouseEvent e) {
 				Client client = null;
 				String serverName = /*ip.getText();*/"127.0.0.1";
 				int portno = 12345;//Integer.parseInt(port.getText());
@@ -42,37 +53,56 @@ public class Start {
 				
 				
 			}
+			
 		});
 		btnNewButton.setBounds(78, 141, 75, 25);
-		btnNewButton.setText("Connect");
 		
-		port = new Text(shell, SWT.BORDER);
 		port.setBounds(78, 58, 76, 21);
-		
-		ip = new Text(shell, SWT.BORDER);
 		ip.setBounds(78, 22, 76, 21);
-		
-		Label ipLbl = new Label(shell, SWT.NONE);
-		ipLbl.setBounds(10, 22, 55, 15);
-		ipLbl.setText("IP Address");
-		
-		Label portLbl = new Label(shell, SWT.NONE);
-		portLbl.setBounds(10, 58, 55, 15);
-		portLbl.setText("Port ");
-		
-		uname = new Text(shell, SWT.BORDER);
 		uname.setBounds(78, 94, 76, 21);
 		
-		Label unameLbl = new Label(shell, SWT.NONE);
+		JLabel ipLbl = new JLabel("IP Address: ");
+		ipLbl.setBounds(10, 22, 55, 15);
+		
+		JLabel portLbl = new JLabel("Port: ");
+		portLbl.setBounds(10, 58, 55, 15);
+		
+		JLabel unameLbl = new JLabel("Username: ");
 		unameLbl.setBounds(10, 94, 55, 15);
-		unameLbl.setText("Username");
 
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
+		container.add(ipLbl);
+		container.add(ip);
+		container.add(portLbl);
+		container.add(port);
+		container.add(unameLbl);
+		container.add(uname);
+		container.add(btnNewButton);
+		
+		frame.setVisible(true);
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
